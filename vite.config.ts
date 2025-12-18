061336d034717fe94469e69379a3b21dae5cc628
jeremym3817 <marino.je@northeastern.edu>
2025-12-18 17:11:24 -0500

    3rd Package iter

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [react()],
   base: "/Jeremy-Marino-Portfolio-Website/",
+  plugins: [react()],
 });
